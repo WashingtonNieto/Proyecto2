1f49bb64c2c1b2b93585793a792a7f80e0328ae9
washington nieto <washington_nieto@hotmail.com>
2026-08-04 21:59:34 -0500

    Actuallizacion Modelos-tablas-sqlite

--- MiAplicacion/models.py
+++ MiAplicacion/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    public = models.BooleanField()
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    create_at = models.DateField()
+
